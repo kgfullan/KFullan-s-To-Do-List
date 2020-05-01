e8901d1738dde96e4d3354cf9470dd0f415fd62c
Katie Fullan <katie.fullan@gmail.com>
2020-05-01 13:36:46 -0500

    Initial Commit

---
+++ KFullan's To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KFullan's To Do List
+//
+//  Created by Kathryn Fullan on 5/1/20.
+//  Copyright © 2020 Kathryn Fullan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KFullan's To Do ListTests/KFullan_s_To_Do_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  KFullan_s_To_Do_ListTests.swift
+//  KFullan's To Do ListTests
+//
+//  Created by Kathryn Fullan on 5/1/20.
+//  Copyright © 2020 Kathryn Fullan. All rights reserved.
+//
+
+import XCTest
+@testable import KFullan_s_To_Do_List
+
+class KFullan_s_To_Do_ListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
